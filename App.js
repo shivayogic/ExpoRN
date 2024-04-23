c096758e9395f22943786ffe1651411e0cd130c3
shivayogi <>
2024-04-23 10:16:11 +0530

    added button

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import Crashes from "appcenter-crashes";
+import Analytics from "appcenter-analytics";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Button 1"
+        onPress={() => {
+          Analytics.trackEvent("Button1", { yogi: "pressed" });
+        }}
+      />
+      <Button
+        title="Button 2"
+        onPress={() => {
+          Analytics.trackEvent("Button2", { yogi: "pressed" });
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
